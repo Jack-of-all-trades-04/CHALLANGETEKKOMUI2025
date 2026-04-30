9cbbfc5664af32acf7f09cb8d842deee52afbddd
Wann97 <mhmd.shofwanaja@gmail.com>
2026-04-30 12:31:59 +0700

    Add message from Muhammad Shofwan

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -2,10 +2,14 @@
 
 int main() {
     printf("\033[1;34m");
-    printf("Written by Tekkom UI 2024\n");
+    printf("Written by Tekkom UI 2025\n");
     printf("\033[0m");
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
-}
+
+    printf("Sukses selalu yaa! jangan sampe nyerah dan berenti dari mimpi luu!!");
+    printf("Jalanin aja duluu, nantii bakal seru seruan kokk");
+
+    printf("FROM: Shofwan'25")
