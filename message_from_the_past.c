351e218a8aac8502728e203c6a3bae660cb7ed89
rzbputra <rzbputra@gmail.com>
2026-04-30 20:02:53 +0700

    Add message from Rizki Bariputra

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -8,4 +8,12 @@ int main() {
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+    
+    printf("From: Hououin Kyouma\n");
+    printf("Yo, diriku di masa depan. Aku, Hououin Kyouma, telah menerima D-Mail mu.\n");
+    printf("Karena ulah organisasi, resource disini terbatas sehingga pesan harus terpisah untuk bisa dikirim.\n");
+    printf("Aku telah menemukannya, lokasi dari ****** ada di *********.\n");
+    printf("Dengan ini, masa lalu, masa kini, dan masa depan berada dalam kepalan tanganku!\n");
+    printf("Aku, ilmuan gila, Hououin Kyouma! dengan ini mendeklarasikan dimulainya Operation Pleiades!\n");
+    printf("Semoga beruntung, diriku. El Psy Kongroo.\n");
 }
