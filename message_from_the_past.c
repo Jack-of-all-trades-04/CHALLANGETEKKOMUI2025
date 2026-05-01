acfcdbfc29ebf837d4a7bcaba640b6b1f7a60b1f
RikoDharmawan <rikodharmawan123@gmail.com>
2026-05-01 15:13:43 +0700

    Add Message From Riko Dharmawan

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -2,12 +2,12 @@
 
 int main() {
     printf("\033[1;34m");
-    printf("Written by Tekkom UI 2024\n");
+    printf("Written by Tekkom UI 2025\n");
     printf("\033[0m");
 
-    printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
-    printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+    printf("Terima Kasih Aslabkuuu\n");
+    printf("Terima Kasih Bimbingannya\n Terima Kasih Pelajarannya\n Terima Kasih atas semuanyaa \n Terima Kasih Aslab ku\n Terima kasih Aslabku\n BH: bnhrd\n\n");
 
-    printf("From: Baek Yi-jin\n");
-    printf("Selamat atas pernikahan mu Na Hee Do!\n");
+    printf("From: bakanista\n");
+    printf("Selamat atas pernikahan EL RUMI \n");
 }
