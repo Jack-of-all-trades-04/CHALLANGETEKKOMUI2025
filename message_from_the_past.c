62e2267cb96fa7c1d206a3505531c5bb981d4a16
Abrar2112 <mabrarher@gmail.com>
2026-05-01 20:40:46 +0700

    Okeyy <Abrar>

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -2,7 +2,7 @@
 
 int main() {
     printf("\033[1;34m");
-    printf("Written by Tekkom UI 2023\n");
+    printf("Written by Tekkom UI 2025\n");
     printf("\033[0m");
 
     printf("Makasih DIGILAB sudah mengajar kita di DSD maupun PROGDAS \n");
