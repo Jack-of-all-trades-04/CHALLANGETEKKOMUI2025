108d6211ba1baad4f076a65dbe22d4be137961c6
ZFrees <119037062+ZFrees@users.noreply.github.com>
2026-05-01 13:21:29 +0700

    Update message_from_the_past.c

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -6,10 +6,10 @@ int main() {
     printf("\033[0m");
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
-    printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+    printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+    printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
 
-    printf("From: Baek Yi-jin\n");
-    printf("Selamat atas pernikahan mu Na Hee Do!\n");
-
+    printf("From: Atmin\n");
+    printf("I Love DigiLab\n");
     printf(":)");
 }
