b368ccc59a2d2468d79f46bccf41886099605646
PotatoWithoutaBrain <kenturboxyz@gmail.com>
2026-05-01 23:55:16 +0700

    Add message from Kenneth Vittorio

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -8,4 +8,8 @@ int main() {
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+
+    printf("Makasi abang-abang dan kakak-kakak aslab sudah membuat matkul DSD dan Progdas menjadi matkul yang menyenangkan dan lebih mudah untuk dimengerti :)\n");
+    printf("                   __ooooooooo__\n              oOOOOOOOOOOOOOOOOOOOOOo\n          oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n       oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n     oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n   oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n  oOOOOOOOOOOO*  *OOOOOOOOOOOOOO*  *OOOOOOOOOOOOo\n oOOOOOOOOOOO      OOOOOOOOOOOO      OOOOOOOOOOOOo\n oOOOOOOOOOOOOo  oOOOOOOOOOOOOOOo  oOOOOOOOOOOOOOo\noOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\noOOOO     OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO     OOOOo\noOOOOOO OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OOOOOOo\n *OOOOO  OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  OOOOO*\n *OOOOOO  *OOOOOOOOOOOOOOOOOOOOOOOOOOOOO*  OOOOOO*\n  *OOOOOO  *OOOOOOOOOOOOOOOOOOOOOOOOOOO*  OOOOOO*\n   *OOOOOOo  *OOOOOOOOOOOOOOOOOOOOOOO*  oOOOOOO*\n     *OOOOOOOo  *OOOOOOOOOOOOOOOOO*  oOOOOOOO*\n       *OOOOOOOOo  *OOOOOOOOOOO*  oOOOOOOOO*\n          *OOOOOOOOo           oOOOOOOOO*\n              *OOOOOOOOOOOOOOOOOOOOO*\n                   ooooooooooooo\n");
+    printf("From: Kenneth Vittorio");
 }
