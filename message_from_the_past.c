fc8a197c6368402755f453db5568f01725f5c3be
zira47 <nadziradaviana7@gmail.com>
2026-05-01 17:19:00 +0700

    menambahkan pesan ke diri sendiri

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,5 +7,8 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
-    printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+    printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n\n");
+
+    printf("Halooo this is zira from 2026 masih semester 2\n");
+    printf("Semoga pas baca ini udah sukses yah biar 5 taun lagi (2031) udah kebeli BMW seri 5 \n");
 }
