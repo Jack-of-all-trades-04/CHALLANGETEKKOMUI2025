2c3d0073e4f981ce3d886d0bdb8ea06b81c8d643
Vixhaf <vinthuxaftrievenhym@gmail.comm>
2026-05-02 14:15:22 +0700

    Bagi kami loker aslab

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,10 +7,7 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
-
-    printf("From: Baek Yi-jin\n");
-    printf("Selamat atas pernikahan mu Na Hee Do!\n\n\n");
-
+    printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n\n\n");
 
     printf("\033[1;34m");
     printf("Written by Vikko Adibratha Teknik Komputer 2025 anggota PASIF IKM FTUI 2025\n");
