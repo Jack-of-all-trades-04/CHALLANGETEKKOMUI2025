3c4d39f3192178d2ded4530061190a96a366d6a2
iqbalrachman-git <rachmaniqbalaulia@gmail.com>
2026-05-02 22:12:53 +0700

    add mesage from iqbal

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -8,4 +8,9 @@ int main() {
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+
+    printf("jujur progdas gua ketok 2 modul ga submit recording dan salah satunya ga submit cs2 pula. Tapi semoga gua ga ngulang praktikum karena jir lah men nangis sih klo ngulang.");
+    printf("\n anyway semoga gua kuat menjalankan kehidupan kuliah ini sampai selesai. (bonus klo misal bisa deket sama dia jadi sama dia HSAHDSAHJDASHD)");
+    
+    return 0;
 }
