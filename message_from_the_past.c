8140f7b65e779b7d8bfac248a2d52c26330b160b
9abriells <gabriellaprscll@gmail.com>
2026-05-02 18:24:35 +0700

    Revise message content in message_from_the_past.c

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -1,34 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("============================================================\n");
-    printf("          A MESSAGE FROM THE GABRIELLA OF 2026              \n");
-    printf("============================================================\n\n");
+    printf("\033[1;34m");
+    printf("Written by Tekkom UI 2024\n");
+    printf("\033[0m");
 
-    printf("To my future self,\n\n");
+    printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
+    printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
 
-    printf("Right now, as I'm typing these lines of code, I am overwhelmed.\n");
-    printf("To be honest, I feel like crying. There were so many times \n");
-    printf("where I thought about giving up because everything felt too \n");
-    printf("heavy, too complicated, and just impossible.\n\n");
-
-    printf("But look at you. You didn't quit.\n");
-    printf("I am so incredibly proud of you for choosing to keep going \n");
-    printf("even when your hands were shaking and your mind was tired.\n");
-    printf("I know that all these struggles are just the foundation for \n");
-    printf("the bright future that is waiting for us.\n\n");
-
-    printf("I also want to remember the people around me right now.\n");
-    printf("I'm so grateful for those who stayed, supported me, and \n");
-    printf("reminded me of who I am when I forgot. I couldn't have \n");
-    printf("made it this far without them.\n\n");
-
-    printf("So, future Gabriella, I hope you're smiling while reading this.\n");
-    printf("Never forget how determined you were today.\n\n");
-
-    printf("With love and pride,\n");
-    printf("Gabriella Priscillia Agustin, Computer Engineering 2025\n");
-    printf("============================================================\n");
-
-    return 0;
+    printf("From: Gabriella Priscillia Agustin\n");
+    printf("Good luck on every journey! Imanuel\n");
 }
