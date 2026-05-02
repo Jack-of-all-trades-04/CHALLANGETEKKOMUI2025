d4e38f2758755e76a9918a37abf11ea8f9cc3ffb
syafiqalh <syafiqalhakim143@gmail.com>
2026-05-02 18:24:47 +0700

    Add message from syafiq

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -8,4 +8,23 @@ int main() {
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+
+    printf("=================================================================================================================\n");
+    printf("from : CR7\n");
+
+    printf("kalau lu baca ini, artinya selamat, lu masih survive di tekkom.");
+    printf("semoga lu enjoy dengan proses yang dijalani, memang ga selalu mulus. tetapi kalo lu bisa nikmati, semua akan terasa mudah. enjoy while it last.");
+    printf("lu mau jadi apapun sekarang, gw bangga sama perjuangan lu.");
+    printf("kalau lu lagi down atau terpuruk, coba take a look back and see how much progress have you made. youre doing good bro!!");
+    printf("semoga sekarang lu bahagia dan mencapai semua impian lu. Banggain orang tua, get a job that you can enjoy doing and obviously huge salary, married with girl of your dream.");
+    printf("semoga pengetahuan keislaman lu bertambah dan jangan lupa diterapkan untuk keseharian. selalu hadirkan Allah di hati lu. be a good guy, ok!!");
+    printf("is it still THAT girl from high school? kalo enggak, selamat lu udah move on! kalo iya, gimana kondisinya? apakah lu dapat chance tersebut?");
+    printf("klo lu dapat chance, make your move, make it happen. kalo enggak ya kayaknya its over bro, pack it up, move on. jangan sedih, Allah pasti kasih yang lebih baik, just make sure YOU ARE A GOOD GUY!");
+    printf("kalo lu hancur, please find a way to comeback. gw tau lu bisa. lu udah berkali-kali ngalamin ini dan akhirnya bisa comeback.");
+    printf("enjoy waktu lu sama temen, terutama keluarga. karena mereka di dunia ini ga abadi.");
+    printf("terakhir, jangan lupa bersyukur. kebahagiaan bukan tentang apa yang lu miliki, tapi apa yang lu syukuri.");
+
+    printf("buat tekkom 25, semoga kalian bisa menjadi rumah bagi gw, dan selalu support ketika gw kesulitan");
+    printf("buat temen temen gw, makasih udah mau nemenin gw berproses, kalian semua jg keren wkwk. semoga bisa menjadi amalan baik yang menolong kalian di akhirat");
+    printf("buat aslab-aslab senior, makasih bang/kak atas ilmunya. semoga ilmu yang diajarkan bisa gw terapkan dan menjadi amal jariyah buat kalian");
 }
