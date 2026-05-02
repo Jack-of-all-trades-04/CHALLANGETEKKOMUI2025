3820b30f8f3ce8f709738eede5e7f10252fc1535
Farrell-cs <farrellalazri@gmail.com>
2026-05-02 17:34:44 +0700

    Add message from Farrell AASY

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -8,4 +8,8 @@ int main() {
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+    
+    printf("\nFrom: Farrell AASY\n");
+    printf("I just hope y'all will be happy and successful by the end of our time here in UI. Also if any of you guys have issues that you guys wanna talk about, then I'm always available :) WYATB!!!!!!\n");
+    
 }
