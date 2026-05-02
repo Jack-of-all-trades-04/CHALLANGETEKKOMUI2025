e552c17d38069f61c67a100e52e6d8cae4d91d1e
carlosssricardo <carlossuneo09@gmail.com>
2026-05-02 15:07:24 +0700

    Add message from Carlos

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
     printf("\n");
-    printf("Hallo Carlos, gimana kuliahnya sejauh ini?\n");
-    printf("Kuliah di tempat yang selalu kamu impikan di massa kecil :3\n");
-    printf("IPK kamu ga hancurkan karena kamu fomo ikut 3 organisasi waktu maba ? wkwkwk\n");
-
-    printf("\nSekarang saat kamu ngetik ini, kamu lagi kebantai habis-habisan loh karena 3 organisasi itu, akdemis, dan kamu jadi Wapj supgladiator wkwk\n");
-    printf("Tapi untungnya kamu punya pacar cantik, pinter, lucu, dan pengertian yang bisa jadi rumah kamu ketika lagi cape\n");
-    printf("BTW dia sering banget loh bantuin tugas-tugas kamu (karena kalian satu jurusan dan satu angkatan wkkw)\n");
-    printf("Aku harap kamu masih bareng dia yaa sampai kamu baca codingan ini wkwkk\n");
-    printf("Karena dulu kita pernh janji buat ga ninggalin dia dan jadiin dia pelabuhan terakhir di cerita cinta hidup ini waokwaowkaok\n");
-
-    printf("\nSemngat ya Carlos! Gua yakin kita bisaa buat keluar dari lingkaran setan itu\n");
-    printf("Seperti perkataan kita dulu, kita harus buat kebahagian buat diri kita sendiri dan ga akan bergantung sama orang lain lagi\n");
+    
+    printf("Terima kasih Aslab Digilab Baik Hati merah senyum\n");
+    printf("Semoga kita ketemu lagi di masa depan sebagai FOUNDER perusahaan gede :3\n");
 
     printf("\n");
     return 0;
