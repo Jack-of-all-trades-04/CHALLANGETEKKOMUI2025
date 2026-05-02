41c70fe66ed2cd20a0c0fcceb819cc7e29614791
harlandyoga <harlandyoga@gmail.com>
2026-05-02 11:42:44 +0700

    Add message from <DeloHarland>

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -8,4 +8,8 @@ int main() {
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+
+    printf("From : Delolaland \n");
+    printf("It’s been a fun semester, even though I definitely threw on this subject. Either way, it was a vibe. Good luck to the grader dealing with those 100 pics of Zeta—my bad! I hope future me is doing better than I am right now, because I’m actually struggling, skskksks.");
+    printf("To my future self: LOCK IN. The job market is cooked right now, especially for our major. Also, huge shoutout to the Aslabs! Thanks for the effort, the fun times, and just being real. Cheers! \n");
 }
