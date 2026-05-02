235698770aeb31342521f42d501757f494b30a1c
Insan K <kamilazzam2@gmail.com>
2026-05-02 23:38:09 +0700

    Add message from Insan

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -8,4 +8,9 @@ int main() {
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+
+    printf("From : Insan\n");
+    printf("Terima kasih banyak abang-abang dan kakak aslab Digilab atas ilmuya, semoga kelak segala urusan abang/kakak sekalian dimudahkan. Aminn\n");
+
+    return 0;
 }
