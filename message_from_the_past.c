ae1c108299fd7a54c031bc6dc22c9fc057196552
anangp7221 <anangpamulang@gmail.com>
2026-05-02 22:48:46 +0700

    Add message from Anang Purwanto

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -6,6 +6,14 @@ int main() {
     printf("\033[0m");
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
-    printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
-    printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+    printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("From: Baek Yi-jin\n");
+    printf("Selamat atas pernikahan mu Na Hee Do!\n");
+    
+    // anang's message
+    printf("From: Anang Purwanto\n");
+    printf("Semoga pas baca ini lagi, kita semua udah sukses dan bahagia di bidang dan specialty kita masing-masing.\n");
+    printf("Shoutout buat my lovely wife Aurel Sipil 25, teman-teman sejawatku yang ku sayangi, semua member siwagagah, Farrell When YH dan Abrar Komin yang udah turut mensukseskan finpro dsd kala itu, dan juga Insan serta Zuhair yang udah jadi teammate gw selama progdas. Semoga kita semua bahagia dan happy yh :D\n");
+    printf("i love you Tekkom UI 2025!\n");
 }
