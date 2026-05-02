3a3b8d8c1d75635e71b855d3528dfba9080ac1fc
YOUR_USERNAME <YOUR_EMAIL_ADDRESS@EXAMPLE.COM>
2026-05-02 21:27:30 +0700

    Menambahkan pesan Fatma

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -2,10 +2,9 @@
 
 int main() {
     printf("\033[1;34m");
-    printf("Written by Tekkom UI 2024\n");
+    printf("Written by Tekkom UI 2025\n");
     printf("\033[0m");
 
-    printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
-    printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
-    printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+printf("From: Fatma Alexa Mustofa\n");
+printf("Semangat terus para aslab gacor, makasih udah mau ngajarin para praktikan dengan sabar walaupun tetep pada pakai ai kl ngerjain tugas :>\n");
 }
