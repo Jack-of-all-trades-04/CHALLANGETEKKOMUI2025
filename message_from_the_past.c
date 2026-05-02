64200470381ec6a4c938197af6491147f720356e
jisjus <88531903+Ajiszs@users.noreply.github.com>
2026-05-02 20:33:17 +0700

    Update message_from_the_past.c

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -8,4 +8,9 @@ int main() {
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+
+    printf("\nFrom: adalah 2025\n");
+    printf("Kalau kamu lagi baca ini, berarti kamu sudah sampai sejauh ini.\n");
+    printf("Semoga semua perjuangan begadang, stress, dan capek dulu tidak sia-sia.\n");
+    printf("Jangan lupa tetap rendah hati dan terus belajar.\n");
 }
