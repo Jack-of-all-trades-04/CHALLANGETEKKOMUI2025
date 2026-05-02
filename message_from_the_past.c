735f3d8d7c913e7a9ed7c067d21e7a1195aa1464
hantuu01 <hana.putri51@ui.ac.id>
2026-05-02 20:00:55 +0700

    Add message from <hana>

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -8,4 +8,14 @@ int main() {
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+    
+    printf("\033[1;34m");
+    printf("Written by hanntu Tekkom UI 2025\n");
+    printf("\033[0m");
+
+    printf("semoga kita masih temenan dan ketemu terus kita bareng bareng sampe sukses.\n");
+    printf("teruntuk kaka kaka aslab makasi selama ini udah sabar sama aku\n");
+    printf("teruntuk temen temen seangkatan semog kita bisa solid terus kedepannya yabisa terus bareng, maapin yee kalo banyak salah\n");
+    printf("teruntuk anomali satu ininihhh yang selalu ngambek semoga kita bisa bareng terus ya love you anomali\n");
+
 }
