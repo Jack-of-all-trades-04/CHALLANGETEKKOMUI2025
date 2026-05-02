9a3066b1bad72d9d4345126b17511b13ae98f246
lanangcakrawala <lanangcakrawalat@gmail.com>
2026-05-02 09:44:31 +0700

    Add message from Introverted

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -9,7 +9,7 @@ int main() {
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
 
-    printf("Written by someone who has extra-introverted personality\n");
+    printf("\nWritten by someone who has extra-introverted personality\n");
     printf("Sebelumnya terima kasih kepada para aslab atas bantuannya selama ini\n");
     printf("Harapan saya, bisa membuat atau menciptakan sesuatu yang berbeda yang positif, memiliki progress yang signifikan dalam kehidupan, keluarga sehat selalu dan bisa membanggakan mereka, memiliki pasangan yang supportif hwhwhw\n");
 }
