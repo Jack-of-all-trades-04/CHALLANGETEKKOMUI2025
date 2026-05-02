2e26777f17afe67e4d2648145c23e98ad34b65a7
Farrell-cs <farrellalazri@gmail.com>
2026-05-02 23:10:10 +0700

    To you, 3(000) Years From Now

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
+#define DELAY() Sleep(5000)
+#else
+#include <unistd.h>
+#define DELAY() sleep(5) 
+#endif
+//Ignore this, it's just for dramatic effect in like 3 years or so
+//P.S. Listen to a sad song or something while you read this you emotional twat
 
 int main() {
     printf("\033[1;34m");
@@ -9,7 +18,79 @@ int main() {
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
     
-    printf("\nFrom: Farrell AASY\n");
-    printf("I just hope y'all will be happy and successful by the end of our time here in UI. Also if any of you guys have issues that you guys wanna talk about, then I'm always available :) WYATB!!!!!!\n");
+    printf("\n\n\n\n\n\n A message for me personally:");
+    DELAY();
     
+    printf("\nYou've always been a smart kid and you know that, hope you're less hesitant on your choices on what to do wherever you are in the future");
+    DELAY();
+    
+    printf("\n\nHope you're still that solid collumn of ego that you always were and always will be, helping people in need rather than asking for it");
+    DELAY();
+    
+    printf("\n\nI know you're gonna do great things eventually, I just don't know how or what though cause as you've probably seen it's been a 180 turn even just from a year ago (2025) and it hasn't turned into a bad thing at all");
+    DELAY();
+    
+    printf("\n\nI hope you continue to larp the things that you enjoy larping, because you know damn well that just means 'fake it until you make it'. Hope that doesn't backfire on you lmfao");
+    DELAY();
+    
+    printf("\n\nI really don't know what I'm writing, it's been a while since I thought about myself so I'm just writing all the things I should've said to myself");
+    DELAY();
+    
+    printf("\n\nAlso I know damn well you're gonna be such a different person from the current you, just look at bang Christian Sutandar. He actually acts like an adult unlike 2026 you lol");
+    DELAY();
+    
+    printf("\n\nSemoga lu jadi aslab or at least dapat magang bagus lah, you deserve that at least");
+    DELAY();
+    
+    printf("\n\nHope all the people you've helped throughout your life has seen positive change and I hope you're happy with seeing them grow as a person. I guess I've already experienced this but hell I just know someone that I helped is gonna be so much more successful than I am, has been happening to me ever since Grade school man");
+    DELAY();
+    
+    printf("\n\nYou're a walking Solomon's Paradox and I lowkey want you to stay that way just cause it would be funny lol");
+    DELAY();
+    
+    printf("\n\nI predict you'll still be single by the end of all this just cause you've always been that way (A solid collumn of ego), would be interesting if you changed tho for once");
+    DELAY();
+    
+    printf("\n\nAlways remember not to put too much pressure on yourself for grades, always remember that 'yang penting tuh ilmunya bukan nilainya'");
+    DELAY();
+    
+    printf("\n\nBut obviously still pressure yourself lol, you barely have any money and how are you gonna better the lives of the people around you without that. So still grind and pressure yourself for everyone around you.");
+    DELAY();
+    
+    printf("\n\nYou've lived in such a way where you always tried and succeeded in proving other people wrong. I hope it comes to a point where it's just you proving yourself (self-doubt) wrong");
+    DELAY();
+    
+    printf("\n\nI know damn well I'm close to that breaking point and I know you've always been motivated to reach that.");
+    DELAY();
+    
+    printf("\n\nSo prove the world that you can do it");
+    DELAY();
+    
+    printf("\n\nBecause you always pulled through someway somehow, I know I could always count on you to do that");
+    DELAY();
+    
+    printf("\n\nIt's only up from here buddy.");
+    DELAY();
+    
+    printf("\n\n\n\n\nLowkey edgy as fuck lmfaooo");
+    DELAY();
+    
+    printf("\nBut you get the gist of it\n");
+    DELAY();
+
+    printf("\nAnyways best of luck for realsies now, love you my future self, from your past self");
+    DELAY();
+
+    printf("\nAlso tell me how all about Genshin ends and how your GF is like (if you're even able to get one lmfaooooooooooooo)");
+    DELAY();
+
+    printf("To You, 3 Years From Now");
+    DELAY();
+
+    printf("From You, 3 Years Ago");
+    DELAY();
+
+    printf("Not as dramatic or that much of a time difference than the actual show but you get the gist. Love you man, continue loving other people as well");
+    //When yh
+    return 0;
 }
