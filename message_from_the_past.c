d5b8810aa8b84efaed65f2ba26137ad6af765ec8
carlosssricardo <carlossuneo09@gmail.com>
2026-05-02 14:55:34 +0700

    Add message from Carlos

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
-int main() {
-    printf("\033[1;34m");
-    printf("Written by Tekkom UI 2024\n");
-    printf("\033[0m");
+int main(void) {
+    printf("\n");
+    printf("Hallo Carlos, gimana kuliahnya sejauh ini?\n");
+    printf("Kuliah di tempat yang selalu kamu impikan di massa kecil :3\n");
+    printf("IPK kamu ga hancurkan karena kamu fomo ikut 3 organisasi waktu maba ? wkwkwk\n");
 
-    printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
-    printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
-    printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+    printf("\nSekarang saat kamu ngetik ini, kamu lagi kebantai habis-habisan loh karena 3 organisasi itu, akdemis, dan kamu jadi Wapj supgladiator wkwk\n");
+    printf("Tapi untungnya kamu punya pacar cantik, pinter, lucu, dan pengertian yang bisa jadi rumah kamu ketika lagi cape\n");
+    printf("BTW dia sering banget loh bantuin tugas-tugas kamu (karena kalian satu jurusan dan satu angkatan wkkw)\n");
+    printf("Aku harap kamu masih bareng dia yaa sampai kamu baca codingan ini wkwkk\n");
+    printf("Karena dulu kita pernh janji buat ga ninggalin dia dan jadiin dia pelabuhan terakhir di cerita cinta hidup ini waokwaowkaok\n");
+
+    printf("\nSemngat ya Carlos! Gua yakin kita bisaa buat keluar dari lingkaran setan itu\n");
+    printf("Seperti perkataan kita dulu, kita harus buat kebahagian buat diri kita sendiri dan ga akan bergantung sama orang lain lagi\n");
+
+    printf("\n");
+    return 0;
 }
